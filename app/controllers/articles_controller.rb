a19a03d28fd4781f754445a435ae72161d9076b9
fumn112026 <fumn112026@gmail.com>
2020-10-26 18:41:58 +0900

    アソシエーションを実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :content)
+    params.require(:article).permit(:title, :content).merge(user_id: current_user.id)
   end
 
 end
