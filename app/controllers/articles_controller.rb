51fd2dd8aa4eff9e5ff7d143bf04464e942a1141
fumn112026 <fumn112026@gmail.com>
2020-10-26 11:36:30 +0900

    記事投稿機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,33 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+
+  validates :title, :content, presence: true
+
 end
