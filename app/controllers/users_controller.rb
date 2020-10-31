79fbcef0bed22455b52c697a71fe2ea652c9c335
fumn112026 <fumn112026@gmail.com>
2020-10-31 10:54:06 +0900

    マイページの実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+  def show
+    @nickname = current_user.nickname
+    @articles = current_user.articles
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
   resources :articles, except: :index
+  resources :users, only: :show
+
 end
