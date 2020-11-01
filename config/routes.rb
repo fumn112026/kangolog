78ec13df41b938b0442f146b892bf7b52c373791
fumn112026 <fumn112026@gmail.com>
2020-11-01 15:21:43 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
-  resources :articles, except: :index
+  resources :articles, except: :index do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 
 end
