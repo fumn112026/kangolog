6806f6b47845b71d3efc5aff70ddfff7429ab34d
fumn112026 <fumn112026@gmail.com>
2020-11-03 00:03:49 +0900

    削除機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,12 @@ class CommentsController < ApplicationController
     redirect_to "/articles/#{comment.article.id}"
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to "/articles/#{comment.article.id}"
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, article_id: params[:article_id])
