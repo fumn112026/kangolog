d4cdf397763f2b04aaf6cb8394d0db0e1abdfeeb
fumn112026 <fumn112026@gmail.com>
2020-11-03 13:09:23 +0900

    move_to_indexアクションを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit]
-  
+  before_action :move_to_index, except: [:index, :show, :search]
+
   def index
     @articles = Article.includes(:user) 
   end
@@ -45,6 +46,10 @@ class ArticlesController < ApplicationController
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
