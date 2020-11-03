3b0f17c0e5a403dc34084aba99d3a6d10fa78726
fumn112026 <fumn112026@gmail.com>
2020-11-03 13:00:31 +0900

    searchメソッドを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
-
   validates :title, :content, presence: true
-
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    return Artcle.all unless search
+    Article.where('title LIKE(?)', "%#{serach}%")
+  end
 end
