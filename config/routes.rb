db927533f94ecfb6b15653299c5afdcf6d7c273f
fumn112026 <fumn112026@gmail.com>
2020-11-03 12:56:48 +0900

    検索フォームを実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root 'articles#index'
   resources :articles, except: :index do
     resources :comments, only: [:create, :edit, :update, :destroy]
+    collection do
+      get 'search'
+    end
   end
+
   resources :users, only: :show
 
 end
