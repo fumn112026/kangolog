f258e86cf835579c6a4799d21fec81c255448b81
fumn112026 <fumn112026@gmail.com>
2020-11-09 17:04:33 +0900

    マイグレーションファイルを追記

--- db/migrate/20201109073319_create_images.rb
+++ db/migrate/20201109073319_create_images.rb
@@ -2,6 +2,7 @@ class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
       t.string :url, null: false
+      t.references :article, foreign_key: true, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(version: 2020_11_09_073319) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "url", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_images_on_article_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema.define(version: 2020_11_09_073319) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "articles"
 end
