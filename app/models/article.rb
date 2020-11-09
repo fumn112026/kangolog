930f17c3501ce4a31b8c8071f965fc7aea70331a
fumn112026 <fumn112026@gmail.com>
2020-11-09 16:41:18 +0900

    アソシエーションを設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   validates :title, :content, presence: true
   belongs_to :user
   has_many :comments
+  has_many :images
 
   def self.search(search)
     return Article.all unless search
