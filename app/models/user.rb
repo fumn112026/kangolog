1688588139442108b9085322527b6eaba4c5c7bf
fumn112026 <fumn112026@gmail.com>
2020-11-26 12:22:29 +0900

    emaiの一意性を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
+  validates :email, uniqueness: true
   
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
