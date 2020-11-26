d61a3fa15b6abc2d64458a3db4c68817624fb0fe
fumn112026 <fumn112026@gmail.com>
2020-11-26 12:05:26 +0900

    ユーザー削除時に関連記事、コメントを削除するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :articles
-  has_many :comments
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_articles, through: :likes, source: :article
 
