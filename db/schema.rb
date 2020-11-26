7ef3e16b1014aedf0dba2e5ef8694251200425b8
fumn112026 <fumn112026@gmail.com>
2020-11-26 13:20:47 +0900

    Usersテーブルにimageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_044906) do
+ActiveRecord::Schema.define(version: 2020_11_26_041918) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_044906) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "nickname"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
