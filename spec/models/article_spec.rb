0a4225d0521511479489b371cb371bc191b26519
fumn112026 <fumn112026@gmail.com>
2020-11-28 17:43:20 +0900

    articleモデルのテストを追記

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 describe Article do
   describe '#create' do
 
+    it "titleとcontent、user_id存在すれば登録できること" do
+      user = create(:user)
+      article = user.articles.build(
+        title: "aaa",
+        content: "aaa",
+      )
+      expect(article).to be_valid
+    end
+
     it "titleがない場合は登録できないこと" do
       article = build(:article, title: nil)
       article.valid?
@@ -14,6 +23,6 @@ describe Article do
       article.valid?
       expect(article.errors[:content]).to include("can't be blank")
     end
-    
+
   end
 end
