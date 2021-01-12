488499a27367cbf509514ba99f89751e1cae43bd
fumn112026 <fumn112026@gmail.com>
2021-01-12 23:23:51 +0900

    デプロイ先サーバーを複製

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,2 @@
-server '54.248.143.29', user: 'ec2-user', roles: %w{app db web}
+server '54.248.143.29', user: 'ec2-user', roles: %w{app db web1}
+server '3.115.13.115', user: 'ec2-user', roles: %w{app db web2}
