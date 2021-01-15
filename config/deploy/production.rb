51c50ffad5971ceb4bd4efe16b93c4fc6c92cf30
fumn112026 <fumn112026@gmail.com>
2021-01-16 00:41:05 +0900

    RDS接続のための修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '54.248.143.29', user: 'ec2-user', roles: %w{app db web}
-server '3.115.13.115', user: 'ec2-user', roles: %w{app db web}
+server '13.230.135.66', user: 'ec2-user', roles: %w{app db web}
