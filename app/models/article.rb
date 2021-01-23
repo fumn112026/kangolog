3f5131cc93178d80c8d90be5a90237e55e6812fd
fumn112026 <fumn112026@gmail.com>
2021-01-23 14:14:34 +0900

    articleモデルを関連づけ

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,11 +5,19 @@ class Article < ApplicationRecord
   has_many :images, dependent: :destroy
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
+  has_many :tag_relations, dependent: :destroy
+  has_many :tag, through: :tag_relations
   accepts_nested_attributes_for :images, allow_destroy: true
-
 
   def self.search(search)
     return Article.all unless search
     Article.where('title LIKE(?)', "%#{search}%")
+  end
+  
+  def save_tags(savearticle_tags)
+    savearticle_tags.each do |new_name|
+      article_tag = Tag.find_or_create_by(name: new_name)
+      self.tag << article_tag
+    end
   end
 end
