d1efd76b62b4194fb47b01d12bb3711dab8c1fcf
fumn112026 <fumn112026@gmail.com>
2021-01-23 14:12:50 +0900

    tagモデル、tag_relationモデルを作成

---
+++ app/models/tag.rb
@@ -0,0 +1,5 @@
+class Tag < ApplicationRecord
+  has_many :tag_relations, dependent: :destroy
+  has_many :articles, through: :tag_relations
+  validates :name, uniqueness: true
+end
---
+++ spec/factories/tag_relations.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :tag_relation do
+    article { nil }
+    tag { nil }
+    name { "MyString" }
+  end
+end
