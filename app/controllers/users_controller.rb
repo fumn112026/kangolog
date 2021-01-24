7ef31d369aa46a669f474e9d170471e49330daed
fumn112026 <fumn112026@gmail.com>
2021-01-24 22:49:48 +0900

    ゲストユーザーは編集できないにアクションを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit]
+  before_action :set_user, only: [:show, :edit, :check_guest]
   before_action :authenticate_user!, except: :show
+  before_action :check_guest, only: [:edit, :update]
 
   def show
     @nickname = @user.nickname
@@ -23,6 +24,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def check_guest
+    if @user.email == "guest@example.com"
+      redirect_to user_path(@user), alert: 'ゲストユーザーは編集できません'
+    end
   end
 
 end
