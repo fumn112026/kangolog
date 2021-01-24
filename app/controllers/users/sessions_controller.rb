641d00139ae4cfd2e1f56e7431232eadc6ad020a
fumn112026 <fumn112026@gmail.com>
2021-01-24 18:43:38 +0900

    sessionsコントローラーを作成

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.nickname = "guest"
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
+  end
+end
